dc2b23873b1c637c81462cd5006dd09cb2a183f6
linyang <lin772207039@gmail.com>
2023-05-16 17:29:55 +0800

    1

--- static/tests/__init__.py
+++ static/tests/__init__.py
@@ -2,3 +2,4 @@ from static.tests.base import BaseAPITestCase
 #from static.tests.test_userinfo import  UserInfoTestCase
 #from static.tests.test_router import RouetrTestCase
 from static.tests.test_menus import MenysTestCase
+from static.tests.test_amplifieryield import TsetAmplifier
